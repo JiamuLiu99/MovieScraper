34083e9048014ab58edb792d0eea1fb565c57183
Jiamu Liu <jiamu421@ucla.edu>
2022-02-08 00:28:13 -0500

    f-5

--- IMDB_scraper/spiders/imdb_spider.py
+++ IMDB_scraper/spiders/imdb_spider.py
@@ -4,17 +4,15 @@ import scrapy
 
 class ImdbSpider(scrapy.Spider):
     name = 'imdb_spider'
-    start_urls = ['https://www.imdb.com/title/tt0106145/']
+    start_urls = ['https://www.imdb.com/title/tt6208148/']
 
     def parse(self, response):
-        cast_page = response.url + 'fullcredits/'
-        yield scrapy.Request(cast_page, self.parse_full_credits)
+        yield scrapy.Request(response.url + 'fullcredits/', self.parse_full_credits)
 
     def parse_full_credits(self, response):
         actor_lists = [a.attrib["href"] for a in response.css("td.primary_photo a")]
         for ele in actor_lists:
-            actor_page = 'https://www.imdb.com' + ele
-            yield scrapy.Request(actor_page, self.parse_actor_page)
+            yield scrapy.Request('https://www.imdb.com' + ele, self.parse_actor_page)
     
     def parse_actor_page(self, response):
         actor_name = response.css('title::text')[0].get().split(' -')[0]
